6dfb6b32c2d4841193c6fe2872a595a1138db108
leonard <perleomix@gmail.com>
2024-03-22 15:05:17 +0300

    debas com

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use iced::alignment::{Horizontal, Vertical};
 use iced::application::Appearance;
+use iced::widget::shader::wgpu::naga::valid;
 use iced::widget::{
-    button, checkbox, column, container, horizontal_rule, horizontal_space, pick_list, row, text,
-    text_input, Column, Container,
+    button, checkbox, column, container, horizontal_rule, horizontal_space, pick_list, radio, row,
+    text, text_editor, text_input, toggler, Column, Container,
 };
 use iced::{
     theme, Alignment, Application, Color, Command, Element, Length, Renderer, Settings, Theme,
 };
-use pagetest::SecondPage;
-
-mod pagetest;
+mod gui;
+use gui::pagetest::SecondPage;
+// mod tablebuilder;
+mod sqlxmod;
 
 pub fn main() -> iced::Result {
     MyApp::run(Settings::default())
@@ -20,6 +22,13 @@ pub struct MyApp {
     text: String,
     page: SecondPage,
     current_view: Views,
+    content: text_editor::Content,
+    command_to_pg: Choice,
+}
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Choice {
+    Execute,
+    Querry,
 }
 
 #[derive(Debug, Clone)]
@@ -28,6 +37,9 @@ pub enum Message {
     InputChanged(String),
     ThemeChanged(Theme),
     PageChanged(Views),
+    Edit(text_editor::Action),
+    RadioSelected(Choice),
+    
 }
 
 #[derive(Debug, Clone)]
@@ -57,6 +69,8 @@ impl Application for MyApp {
                 text: String::new(),
                 theme: Theme::default(),
                 current_view: Views::Main,
+                content: text_editor::Content::new(),
+                command_to_pg: Choice::Execute,
             },
             Command::none(),
         )
@@ -66,6 +80,7 @@ impl Application for MyApp {
         match message {
             Message::ButtonPressed => {
                 println!("{}", self.text);
+                sqlxmod::process(self.text.clone(), &self.command_to_pg).unwrap();
                 // sqlreq::make(&self.text).unwrap();
                 self.text = String::new()
             }
@@ -77,6 +92,15 @@ impl Application for MyApp {
                 self.theme = theme;
             }
             Message::PageChanged(value) => self.current_view = value,
+            Message::Edit(action) => {
+                self.content.perform(action);
+            }
+            Message::RadioSelected(Choice::Execute) => {
+                self.command_to_pg = Choice::Execute;
+            }
+            Message::RadioSelected(Choice::Querry) => {
+                self.command_to_pg = Choice::Querry;
+            }
         }
         Command::none()
     }
@@ -85,30 +109,48 @@ impl Application for MyApp {
         let choose_theme = pick_list(Theme::ALL, Some(&self.theme), Message::ThemeChanged);
 
         let buttone = button("Subbmit").on_press(Message::ButtonPressed);
-        let button1 = button("menu").on_press(Message::PageChanged(Views::Second));
-        let button2 = button("config").on_press(Message::ButtonPressed);
-        let button3 = button("clients").on_press(Message::ButtonPressed);
-        let textinput = text_input("Type something...", &self.text)
+        let pager = button("menu").on_press(Message::PageChanged(Views::Second));
+    
+
+        let radio_but_exec = radio(
+            "exequte",
+            Choice::Execute,
+            Some(self.command_to_pg),
+            Message::RadioSelected,
+        );
+        let radio_but_querry = radio(
+            "querry",
+            Choice::Querry,
+            Some(self.command_to_pg),
+            Message::RadioSelected,
+        );
+        let textinput = text_input("Напиши свой запрос...", &self.text)
             // .line_height(text::LineHeight::Relative(1.75))
             .on_input(Message::InputChanged)
             .on_submit(Message::ButtonPressed);
+        let row = row![radio_but_exec, radio_but_querry]
+            .padding(10)
+            .spacing(10);
+        let querry_builder = text_editor(&self.content).on_action(Message::Edit);
+        let position = {
+            let (line, column) = self.content.cursor_position();
+            text(format!("{}:{}", line, column))
+        };
 
-        let status_bar = row![
-            choose_theme.padding(5),
-            button1.padding(5),
-            button2.padding(5),
-            button3.padding(5),
-            buttone.padding(5)
-        ]
-        .spacing(2);
+        let status_bar = row![choose_theme.padding(5), buttone.padding(5), pager.padding(5)].spacing(2);
 
         let content = Container::new(
             column![
-                horizontal_space(),
                 status_bar.align_items(Alignment::Start),
                 textinput.padding(10),
+                row,
+                querry_builder.padding(10),
+                position
+                    .vertical_alignment(Vertical::Bottom)
+                    .horizontal_alignment(Horizontal::Left),
             ]
-            .spacing(10),
+            .spacing(10)
+            .padding(10),
         );
         match self.current_view {
             Views::Main => content.into(),
--- src/sqlxmod.rs
+++ src/sqlxmod.rs
@@ -1,23 +1,73 @@
-use sqlx::query;
-use sqlx::Row;
+use entity::book::{self, ActiveModel, Entity as Book};
+use sea_orm::ActiveValue::{NotSet, Set, Unchanged};
+use sea_orm::{prelude::Decimal, ActiveValue, Database, DatabaseConnection, EntityTrait};
+use sea_orm::{
+    ActiveModelTrait, ConnectionTrait, DatabaseBackend, ExecResult, QueryResult, Statement,
+};
+// use tokio_postgres::{Error, NoTls};
+use crate::Choice;
 
-/// How long to sit in the listen loop before exiting.
-///
-/// This ensures the example eventually exits, which is required for automated testing.
-// const LISTEN_DURATION: Duration = Duration::from_secs(5);
-
+use std::error::Error;
 #[tokio::main]
-async fn make() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Building PG pool.");
-    let conn_str =
-        std::env::var("DATABASE_URL").expect("Env var DATABASE_URL is required for this example.");
-    let pool = sqlx::PgPool::connect(&conn_str).await?;
+pub async fn process(request: String, choice: &Choice) -> Result<(), Box<dyn Error>> {
+    let db: DatabaseConnection =
+        Database::connect("postgres://postgres:postgres@localhost/diesel_demo").await?;
 
-    let res: Vec<sqlx::postgres::PgRow> = query(
-        "INSERT INTO genre (genre_id,name_genre) 
-        VALUES (1,'Роман');",
-    )
-    .fetch_all(&pool)
-    .await?;
+    match choice {
+        Choice::Execute => {
+            let exec_res: ExecResult = db
+                .execute(Statement::from_string(DatabaseBackend::Postgres, &request))
+                .await?;
+        }
+        Choice::Querry => {
+            let query_res_vec: Vec<QueryResult> = db
+                .query_all(Statement::from_string(DatabaseBackend::Postgres, &request))
+                .await?;
+            for i in query_res_vec {
+                println!("{:?}", i);
+            }
+        }
+    }
+    // let cheese: Option<<Book as EntityTrait>::Model> = Book::find_by_id(2).one(&db).await?;
+    // println!("{:?}", );
+    // cheese.unwrap().display();
+    // let books: Vec<<Book as EntityTrait>::Model> = Book::find().all(&db).await?;
+
+    // Book insertion
+    // let pear = book::ActiveModel {
+    //     book_id: ActiveValue::Set(14),
+    //     title: ActiveValue::Set(Some("War and peace".to_string())),
+    //     author: ActiveValue::Set(Some(apauthor)),
+    //     price: ActiveValue::Set(Decimal::from_f32_retain(12.3)),
+    //     amount: ActiveValue::Set(Some(12)),
+    // };
+    // let pear: book::Model = pear.insert(&db).await?;
+
+    // pear.insert(&db).await?;
+
+    // Connect to the database.
+    // let (client, connection) = tokio_postgres::connect(
+    //     "host=localhost user=postgres password='postgres' dbname=diesel_demo",
+    //     NoTls,
+    // )
+    // .await?;
+
+    // // The connection object performs the actual communication with the database,
+    // // so spawn it off to run on its own.
+    // tokio::spawn(async move {
+    //     if let Err(e) = connection.await {
+    //         eprintln!("connection error: {}", e);
+    //     }
+    // });
+
+    // // Now we can execute a simple statement that just returns its parameter.
+    // let rows = client.query(&apauthor, &[]).await?;
+
+    // // And then check that we got back the same string we sent over.
+    // // let value: &str = rows[0].get(0);
+    // for i in rows {
+    //     println!("{:?}", i);
+    // }
+
     Ok(())
 }
